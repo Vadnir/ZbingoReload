dd778d65e9de10101d0e10193a8f9e7dec2ff7ce
Vadnir <87151072+Vadnir@users.noreply.github.com>
2023-05-10 19:06:05 -0400

    Add files via upload

--- src/main/java/cl/vadnir/zbingoreload/Managers/PlayerManager.kt
+++ src/main/java/cl/vadnir/zbingoreload/Managers/PlayerManager.kt
@@ -1,109 +1,80 @@
 package cl.vadnir.zbingoreload.Managers
 
 import cl.vadnir.zbingoreload.Profiles.PlayerProfile
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
-class PlayerManager(private val playerList: ArrayList<PlayerProfile> = ArrayList()) {
+class PlayerManager(private val playerList: HashMap<String, PlayerProfile> = hashMapOf()) {
 
     private fun hasPlayer(player: Player): Boolean{
-
-        Bukkit.getConsoleSender().sendMessage(
-            "${ this.playerList }  $player"
-        )
-
-        for ( player1 in this.playerList ) {
-            if (player.name == player1.getName()) {
-                return true
-            }
-        }
-        return false
+        return this.playerList.containsKey(player.name)
     }
 
     private fun hasPlayer(player: PlayerProfile): Boolean{
-        Bukkit.getConsoleSender().sendMessage(
-            "${ this.playerList.toString() }  $player"
-        )
-
-        for ( player1 in this.playerList ) {
-            if (player1.getName() == player.getName()) {
-                return true
-            }
-        }
-        return false
+        return this.playerList.containsKey(player.getName())
     }
 
     private fun hasPlayer(playerName: String): Boolean{
 
-        Bukkit.getConsoleSender().sendMessage(
-            "${ this.playerList.toString() }  $playerName"
-        )
-
-        for ( player in this.playerList ) {
-            if (playerName == player.getName()) {
-                return true
-            }
-        }
-        return false
+        return this.playerList.containsKey(playerName)
     }
 
     fun getAllPlayers(): List<PlayerProfile> {
-        return this.playerList
+        return this.playerList.values.toList()
     }
 
     fun getPlayer(playerName: String): PlayerProfile? {
         if(!hasPlayer(playerName)){
             throw Exception("This player $playerName not has a PlayerProfile")
         }
-        return this.playerList.firstOrNull { it.getName() == playerName }
+        return this.playerList[playerName]
     }
 
     fun getPlayer(player: Player): PlayerProfile? {
         if(!hasPlayer(player)){
             throw Exception("This player $player not has a PlayerProfile")
         }
-        return this.playerList.firstOrNull { it.getName() == player.name }
+        return this.playerList[player.name]
     }
 
     fun createPlayer(player: Player) {
         if(hasPlayer(player)){
             throw Exception("This player has a PlayerProfile")
         }
-        this.playerList.add(PlayerProfile(player.name, ""))
+        this.playerList[player.name] = PlayerProfile(player.name)
     }
 
     fun createPlayer(playerName: String) {
         if(hasPlayer(playerName)){
             throw Exception("This player has a PlayerProfile")
         }
-        this.playerList.add(PlayerProfile(playerName, ""))
+        this.playerList[playerName] = PlayerProfile(playerName)
     }
 
     fun addPlayer(player: PlayerProfile){
         if(hasPlayer(player)){
             throw Exception("This player has a PlayerProfile")
         }
-        this.playerList.add(player)
+        this.playerList[player.getName()] = player
     }
 
     fun removePlayer(player: PlayerProfile) {
         if(!hasPlayer(player)){
             throw Exception("This player not has a PlayerProfile")
         }
-        this.playerList.remove(player)
+        this.playerList.remove(player.getName())
     }
 
     fun removePlayer(player: Player) {
         if(!hasPlayer(player)){
             throw Exception("This player not has a PlayerProfile")
         }
-        this.playerList.remove(this.getPlayer(player))
+        this.playerList.remove(player.name)
     }
 
     fun removePlayer(playerName: String) {
         if(!hasPlayer(playerName)){
             throw Exception("This player not has a PlayerProfile")
         }
-        this.playerList.remove(this.getPlayer(playerName))
+        this.playerList.remove(playerName)
     }
 }
